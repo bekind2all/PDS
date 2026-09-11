88757208d95295ea1c162346533474b9eb1df512
feelyoworks <152934711+bekind2all@users.noreply.github.com>
2026-09-11 09:41:54 +0900

    Add PDS service worker

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = 'pds-daily-v5';
+const STATIC_ASSETS = ['/', '/styles.css', '/app.js', '/manifest.webmanifest', '/icons/icon-192.svg', '/icons/icon-512.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (request.method !== 'GET' || url.pathname.startsWith('/api/')) return;
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(request, clone));
+        return response;
+      })
+      .catch(() => caches.match(request).then((cached) => cached || caches.match('/'))),
+  );
+});
